7f6ac2ed90a3ffcdbf693b76cad15d4e8d587cbc
MARLON MARTINS <82619671+marlondmartins@users.noreply.github.com>
2025-03-10 23:21:39 +0000

    Declare variaveis

---
+++ variaveis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char razao_social[100] = "marlon";
+    char cnpj_cpf[20] = "61684608376";
+    int idade;
+    float peso;
+
+    idade = 25;
+    peso = 90; 
+}
